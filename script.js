e081c44e4411956250c8f2ce14b6661396e0c3ab
mwariz <maria.euaires18@gmail.com>
2024-09-23 16:28:14 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function deleteLast() {
+    let display = document.getElementById('display').value;
+    document.getElementById('display').value = display.substring(0, display.length - 1);
+}
+
+function calculate() {
+    let display = document.getElementById('display').value;
+    try {
+        document.getElementById('display').value = eval(display);
+    } catch (error) {
+        document.getElementById('display').value = 'Erro';
+    }
+}
